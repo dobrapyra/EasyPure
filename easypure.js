db6d261663f8a4cf806498b5ccbc0bd8c3d2be49
dobrapyra <michal.zielinski@crafton.pl>
2017-07-16 11:13:26 +0200

    getOffset scroll optional - withScroll param

--- easypure.js
+++ easypure.js
@@ -71,11 +71,15 @@
 
 	// others
 
-	Element.prototype.getOffset = function(relEl){
+	Element.prototype.getOffset = function(relEl, withScroll){
 		var el, offset = { l: 0, t: 0 };
 		for( el = this; el && el !== relEl; el = el.offsetParent ){
-			offset.l += ( el.offsetLeft - el.scrollLeft );
-			offset.t += ( el.offsetTop - el.scrollTop );
+			offset.l += el.offsetLeft;
+			offset.t += el.offsetTop;
+			if( withScroll ){
+				offset.l -= el.scrollLeft;
+				offset.t -= el.scrollTop;
+			}
 		}
 		return offset;
 	};
